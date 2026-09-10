ce43aaf64fe1752341577ade341eb2438202c1da
hanfanglin <51694644+hanfanglin@users.noreply.github.com>
2026-09-10 17:46:54 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,14 @@
 // 离线缓存：让 App 在 iPhone 主屏打开时无需联网也可使用
-const CACHE='profit-v2';
+const CACHE='profit-v3';
+// index.html 排第一：就算后面的文件缓存失败，也要保证离线能打开主程序
 const FILES=['index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE).then(async c=>{
+    // 逐个缓存并容错：addAll 只要有一个文件 404 就会整体失败，导致完全无法离线
+    for(const f of FILES){
+      try{ await c.add(new Request(f,{cache:'reload'})); }catch(err){}
+    }
+  }).catch(()=>{}).then(()=>self.skipWaiting()));
 });
 self.addEventListener('activate',e=>{
   e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
